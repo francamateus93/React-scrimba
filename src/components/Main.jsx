5342bfbd93e5f82a4cf7eec0446dc68c1741f407
francamateus93 <francamateus@gmail.com>
2024-12-11 13:59:44 +0100

    React Project Completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,13 @@
 export default function Main() {
-  return (<h2>Main Content here</h2>)
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Has well over 100K starts in Github</li>
+        <li>Is maintained by Meta</li>
+        <li>React Isn’t a Framework, it’s technically a library</li>
+      </ul>
+    </main>    
+  )
 }
