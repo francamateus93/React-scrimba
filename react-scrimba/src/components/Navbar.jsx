b2acb863f42dee850c0941ad661a52b66b0ea8b4
francamateus93 <francamateus@gmail.com>
2024-12-11 13:35:10 +0100

    Styling Navbar

---
+++ react-scrimba/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="./\src\components\React-logo.png" alt="React Logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
